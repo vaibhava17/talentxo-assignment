adb477e228dadb58f18f389e13117f16ffc943ee
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2023-08-08 16:47:04 +0530

    initial layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import Button from './components/Button';
+import Dropdown from './components/Dropdown';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Col, Row } from 'react-bootstrap';
+
+let listData = [
+  { label: 'Item 1', onClick: (e) => console.log('Item 1 clicked!', e.target.id), id: "item1" },
+  { label: 'Item 2', onClick: (e) => console.log('Item 2 clicked!', e.target.id), id: "item2" },
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Row>
+        <Col span={12}>
+          <Button
+            label="Add New Item to List"
+            className="mx-2 mt-2"
+            onClick={() => console.log('Clicked!')}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col span={12}>
+          <Dropdown
+            menu={listData}
+            className="mx-2 mt-2"
+          />
+        </Col>
+      </Row>
     </div>
   );
 }
